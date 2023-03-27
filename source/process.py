9c70f05f6b21b80c89a99fb159261e083aa660a9
cchaniotaki <cchaniotaki@ubitech.eu>
2023-03-27 13:30:20 +0100

    change the script to get top 1000 projects per forks and per stars for Java, Python, C, C++ and C#

--- source/process.py
+++ source/process.py
@@ -5,52 +5,24 @@ import pandas as pd
 from common import get_graphql_data, write_text, write_ranking_repo
 import inspect
 
-# languages = ['Python']  # For test
-# languages_md = ['Python']  # For test
-# table_of_contents = """
-# * [Python](#python)"""  # For test
-languages = ["ActionScript", "C", "CSharp", "CPP", "Clojure", "CoffeeScript", "CSS", "Dart", "DM", "Elixir", "Go", "Groovy", "Haskell", "HTML", "Java",
-             "JavaScript", "Julia", "Kotlin", "Lua", "MATLAB", "Objective-C", "Perl", "PHP", "PowerShell", "Python", "R", "Ruby", "Rust", "Scala", "Shell",
-             "Swift", "TeX", "TypeScript", "Vim-script"]
+languages = ["Java", "Python", "C", "CPP", "CSharp"]
+# languages = ["Java", "Python"]
 # Escape characters in markdown like # + - etc
-languages_md = ["ActionScript", "C", "C\#", "C\+\+", "Clojure", "CoffeeScript", "CSS", "Dart", "DM", "Elixir", "Go", "Groovy", "Haskell", "HTML", "Java",
-                "JavaScript", "Julia", "Kotlin", "Lua", "MATLAB", "Objective\-C", "Perl", "PHP", "PowerShell", "Python", "R", "Ruby", "Rust", "Scala", "Shell",
-                "Swift", "TeX", "TypeScript", "Vim script"]
+languages_md = ["Java", "Python", "C", "C\+\+", "C\#", ]
+# languages_md = ["Java", "Python"]
 table_of_contents = """
-* [ActionScript](#actionscript)
-* [C](#c)
-* [C\#](#c-1)
-* [C\+\+](#c-2)
-* [Clojure](#clojure)
-* [CoffeeScript](#coffeescript)
-* [CSS](#css)
-* [Dart](#dart)
-* [DM](#dm)
-* [Elixir](#elixir)
-* [Go](#go)
-* [Groovy](#groovy)
-* [Haskell](#haskell)
-* [HTML](#html)
 * [Java](#java)
-* [JavaScript](#javascript)
-* [Julia](#julia)
-* [Kotlin](#kotlin)
-* [Lua](#lua)
-* [MATLAB](#matlab)
-* [Objective\-C](#objective-c)
-* [Perl](#perl)
-* [PHP](#php)
-* [PowerShell](#powershell)
 * [Python](#python)
-* [R](#r)
-* [Ruby](#ruby)
-* [Rust](#rust)
-* [Scala](#scala)
-* [Shell](#shell)
-* [Swift](#swift)
-* [TeX](#tex)
-* [TypeScript](#typeScript)
-* [Vim script](#vim-script)"""
+* [C](#c)
+* [C\+\+](#c-2)
+* [C\#](#c-1)
+"""
+
+
+# table_of_contents = """
+# * [Java](#java)
+# * [Python](#python)
+# """
 
 
 class ProcessorGQL(object):
@@ -94,10 +66,10 @@ class ProcessorGQL(object):
         }
         """
         self.bulk_size = 50
-        self.bulk_count = 2
-        self.gql_stars = self.gql_format % ("stars:>1000 sort:stars", self.bulk_size, "%s")
-        self.gql_forks = self.gql_format % ("forks:>1000 sort:forks", self.bulk_size, "%s")
+        self.bulk_count = 20  # we want 1000. lets start with 500
         self.gql_stars_lang = self.gql_format % ("language:%s stars:>0 sort:stars", self.bulk_size, "%s")
+        # get forks per language
+        self.gql_forks_lang = self.gql_format % ("language:%s forks:>0 sort:forks", self.bulk_size, "%s")
 
         self.col = ['rank', 'item', 'repo_name', 'stars', 'forks', 'language', 'repo_url', 'username', 'issues',
                     'last_commit', 'description']
@@ -132,58 +104,54 @@ class ProcessorGQL(object):
         return repos
 
     def get_all_repos(self):
-        # get all repos of most stars and forks, and different languages
-        print("Get repos of most stars...")
-        repos_stars = self.get_repos(self.gql_stars)
-        print("Get repos of most stars success!")
-
-        print("Get repos of most forks...")
-        repos_forks = self.get_repos(self.gql_forks)
-        print("Get repos of most forks success!")
 
         repos_languages = {}
         for lang in languages:
             print("Get most stars repos of {}...".format(lang))
             repos_languages[lang] = self.get_repos(self.gql_stars_lang % (lang, '%s'))
             print("Get most stars repos of {} success!".format(lang))
-        return repos_stars, repos_forks, repos_languages
+
+        repos_forks_languages = {}
+        for lang in languages:
+            print("Get most forks repos of {}...".format(lang))
+            repos_forks_languages[lang] = self.get_repos(self.gql_forks_lang % (lang, '%s'))
+            print(repos_forks_languages[lang][0])
+            print("Get most forks repos of {} success!".format(lang))
+
+        return repos_languages, repos_forks_languages
 
 
 class WriteFile(object):
-    def __init__(self, repos_stars, repos_forks, repos_languages):
-        self.repos_stars = repos_stars
-        self.repos_forks = repos_forks
+    def __init__(self, repos_languages, forks_languages):
         self.repos_languages = repos_languages
+        self.forks_languages = forks_languages
         self.col = ['rank', 'item', 'repo_name', 'stars', 'forks', 'language', 'repo_url', 'username', 'issues',
                     'last_commit', 'description']
-        self.repo_list = []
-        self.repo_list.extend([{
-            "desc": "Stars",
-            "desc_md": "Stars",
-            "title_readme": "Most Stars",
-            "title_100": "Top 100 Stars",
-            "file_100": "Top-100-stars.md",
-            "data": repos_stars,
-            "item": "top-100-stars",
-        }, {
-            "desc": "Forks",
-            "desc_md": "Forks",
-            "title_readme": "Most Forks",
-            "title_100": "Top 100 Forks",
-            "file_100": "Top-100-forks.md",
-            "data": repos_forks,
-            "item": "top-100-forks",
-        }])
+        self.star_languages = []
+        self.fork_languages = []
+        if len(repos_languages) > 0:
+            for i in range(len(languages)):
+                lang = languages[i]
+                lang_md = languages_md[i]
+                self.star_languages.append({
+                    "desc": "Forks",
+                    "desc_md": "Forks",
+                    "title_readme": lang_md,
+                    "title_1000": f"Top 1000 Stars in {lang_md}",
+                    "file_1000": f"{lang}-stars.md",
+                    "data": repos_languages[lang],
+                    "item": lang,
+                })
         for i in range(len(languages)):
             lang = languages[i]
             lang_md = languages_md[i]
-            self.repo_list.append({
+            self.fork_languages.append({
                 "desc": "Forks",
                 "desc_md": "Forks",
                 "title_readme": lang_md,
-                "title_100": f"Top 100 Stars in {lang_md}",
-                "file_100": f"{lang}.md",
-                "data": repos_languages[lang],
+                "title_1000": f"Top 1000 Forks in {lang_md}",
+                "file_1000": f"{lang}-forks.md",
+                "data": forks_languages[lang],
                 "item": lang,
             })
 
@@ -192,33 +160,40 @@ class WriteFile(object):
         # write the head and contents of README.md
         write_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
         head_contents = inspect.cleandoc("""[Github Ranking](./README.md)
-            ==========
-
-            **A list of the most github stars and forks repositories.**
-
-            *Last Automatic Update Time: {write_time}*
-
-            ## Table of Contents
-
-            * [Most Stars](#most-stars)
-            * [Most Forks](#most-forks)""".format(write_time=write_time)) + table_of_contents
+                ==========
+    
+                **A list of the most github stars and forks repositories.**
+    
+                *Last Update Time: {write_time}*
+    
+                ## Table of Contents
+    
+                """.format(write_time=write_time)) + table_of_contents
         write_text("../README.md", 'w', head_contents)
 
     def write_readme_lang_md(self):
-        os.makedirs('../Top100', exist_ok=True)
-        for repo in self.repo_list:
-            # README.md
-            title_readme, title_100, file_100, data = repo["title_readme"], repo["title_100"], repo["file_100"], repo["data"]
-            write_text('../README.md', 'a',
-                       f"\n## {title_readme}\n\nThis is top 10, for more click **[{title_100}](Top100/{file_100})**\n\n")
-            write_ranking_repo('../README.md', 'a', data[:10])
-            print(f"Save {title_readme} in README.md!")
+        os.makedirs('../Top1000', exist_ok=True)
+        for repo in self.star_languages:
+            WriteFile.write_file(repo)
+        for repo in self.fork_languages:
+            WriteFile.write_file(repo)
 
-            # Top 100 file
-            write_text(f"../Top100/{file_100}", "w",
-                       f"[Github Ranking](../README.md)\n==========\n\n## {title_100}\n\n")
-            write_ranking_repo(f"../Top100/{file_100}", 'a', data)
-            print(f"Save {title_100} in Top100/{file_100}!\n")
+    @staticmethod
+    def write_file(repo):
+        # README.md
+        title_readme, title_1000, file_1000, data = repo["title_readme"], repo["title_1000"], repo["file_1000"], \
+            repo[
+                "data"]
+        write_text('../README.md', 'a',
+                   f"\n## {title_readme}\n\nThis is top 10, for more click **[{title_1000}](Top1000/{file_1000})**\n\n")
+        write_ranking_repo('../README.md', 'a', data[:10])
+        print(f"Save {title_readme} in README.md!")
+
+        # Top 1000 file forks
+        write_text(f"../Top1000/{file_1000}", "w",
+                   f"[Github Ranking](../README.md)\n==========\n\n## {title_1000}\n\n")
+        write_ranking_repo(f"../Top1000/{file_1000}", 'a', data)
+        print(f"Save {title_1000} in Top1000/{file_1000}!\n")
 
     def repo_to_df(self, repos, item):
         # prepare for saving data to csv file
@@ -231,16 +206,29 @@ class WriteFile(object):
         return pd.DataFrame(repos_list, columns=self.col)
 
     def save_to_csv(self):
-        # save top100 repos info to csv file in Data/github-ranking-year-month-day.md
-        df_all = pd.DataFrame(columns=self.col)
-        for repo in self.repo_list:
+        # save top1000 repos info to csv file in Data/github-ranking-year-month-day.md
+
+        for repo in self.star_languages:
+            print(f"Star Languages CSV {repo['item']}")
+            df_all = pd.DataFrame(columns=self.col)
             df_repos = self.repo_to_df(repos=repo["data"], item=repo["item"])
             df_all = df_all.append(df_repos, ignore_index=True)
+            save_date = datetime.utcnow().strftime("%Y-%m-%d")
+            os.makedirs('../Data', exist_ok=True)
+            df_all.to_csv('../Data/github-star-ranking-' + save_date + '-' + repo["item"] + '.csv', index=False,
+                          encoding='utf-8')
+            print('Save data to Data/github-star-ranking-' + save_date + '-' + repo["item"] + '.csv')
 
-        save_date = datetime.utcnow().strftime("%Y-%m-%d")
-        os.makedirs('../Data', exist_ok=True)
-        df_all.to_csv('../Data/github-ranking-' + save_date + '.csv', index=False, encoding='utf-8')
-        print('Save data to Data/github-ranking-' + save_date + '.csv')
+        for repo in self.fork_languages:
+            print(f"Fork Languages CSV {repo['item']}")
+            df_all = pd.DataFrame(columns=self.col)
+            df_repos = self.repo_to_df(repos=repo["data"], item=repo["item"])
+            df_all = df_all.append(df_repos, ignore_index=True)
+            save_date = datetime.utcnow().strftime("%Y-%m-%d")
+            os.makedirs('../Data', exist_ok=True)
+            df_all.to_csv('../Data/github-fork-ranking-' + save_date + '-' + repo["item"] + '.csv', index=False,
+                          encoding='utf-8')
+            print('Save data to Data/github-fork-ranking-' + save_date + '-' + repo["item"] + '.csv')
 
 
 def run_by_gql():
@@ -248,8 +236,10 @@ def run_by_gql():
     os.chdir(os.path.join(ROOT_PATH, 'source'))
 
     processor = ProcessorGQL()  # use Github GraphQL API v4
-    repos_stars, repos_forks, repos_languages = processor.get_all_repos()
-    wt_obj = WriteFile(repos_stars, repos_forks, repos_languages)
+    repos_languages, repos_forks_languages = processor.get_all_repos()
+    print("Retrieve all repos. Start writing file")
+    wt_obj = WriteFile(repos_languages, repos_forks_languages)
+    wt_obj.write_head_contents()
     wt_obj.write_head_contents()
     wt_obj.write_readme_lang_md()
     wt_obj.save_to_csv()
